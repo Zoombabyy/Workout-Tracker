4f4e803bee5b7496178c14af17353dfb024cea7d
unknown <travissaucedo2514@gmail.com>
2021-12-01 17:00:01 -0600

    added some boiler code, more code to come

---
+++ Develop/routes/htmlroutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {});
+
+router.get("/api/workouts/range", (req, res) => {});
+
+router.post("/api/workouts", (req, res) => {});
+
+router.post("/api/workouts/range", (req, res) => {});
+
+router.put("/api/workouts/:id", (req, res) => {});
+
+module.exports = router;
